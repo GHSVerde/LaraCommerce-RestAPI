20314095f6534baf43ef92fdcdb9f33d21aa4c9a
Gabriel Verde <gabrielhsverde@outlook.com>
2019-11-15 12:21:06 -0200

    Product Collection Resource Created

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -12,9 +12,9 @@ class ReviewController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index($id)
     {
-        //
+        return \App\Model\Product::find($id)->reviews;
     }
 
     /**
--- app/Http/Resources/Product/ProductResource.php
+++ app/Http/Resources/Product/ProductResource.php
@@ -15,15 +15,16 @@ class ProductResource extends JsonResource
     public function toArray($request)
     {
         return [
-            'name' => $this->name,
+            'name'        => $this->name,
             'description' => $this->detail,
-            'price' => $this->price,
-            'stock' => ($this->stock == 0) ? 'Out of Stock' : $this->stock,
-            'discount' => $this->discount,
+            'price'       => $this->price,
+            'stock'       => ($this->stock == 0) ? 'Out of Stock' : $this->stock,
+            'discount'    => $this->discount,
             'total_price' => round(( 1 - ($this->discount/100))* $this->price,2),
-            'rating' => $this->reviews->count() > 0 ? round($this->reviews->sum('star')/$this->reviews->count(),2) : 'No Ratings Yet',
-            'href' => [
+            'rating'      => $this->reviews->count() > 0 ? round($this->reviews->sum('star')/$this->reviews->count(),2) : 'No Ratings Yet',
+            'href'        => [
                 'reviews' => route('reviews.index', $this->id)
+                // 'reviews' => \App\Model\Product::find($this->id)->reviews
             ]
         ];
     }
